8f3de2189dbac4c108ed0dd9fca1d4da6250fc19
nhat191024 <richberchannel01@gmail.com>
2024-07-25 09:03:37 +0700

    feat: Seed database with initial data for users, banners, promotions, categories, rooms, and bills

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Models\banner;
+use App\Models\bill;
+use App\Models\category;
+use App\Models\promotion;
+use App\Models\room;
 use App\Models\User;
+// use Database\Factories\BannerFactory;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,10 +20,19 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $jsonFilePath = "./database/seeders/data.json";
+        $jsonContent = file_get_contents($jsonFilePath);
+        $dataArray = json_decode($jsonContent, true);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->count(10)->create();
+        banner::factory()->count(10)->create();
+        promotion::factory()->count(10)->create();
+
+        foreach ($dataArray['category'] as $data) {
+            category::create($data);
+        }
+
+        room::factory()->count(10)->create();
+        bill::factory()->count(10)->create();
     }
 }
