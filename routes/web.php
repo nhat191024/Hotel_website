fb6453844279fdcf9de87bd027424ed39e2b236a
FuminoLap <richberchannel01@gmail.com>
2024-08-01 11:08:17 +0700

    Refactor: Update web.php routes for admin category CRUD operations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+
+    Route::prefix('/category')->group(function () {
+        Route::get('/', [CategoryController::class, 'index'])->name('admin.category.index');
+        Route::get('/add', [CategoryController::class, 'showAddCategory'])->name('admin.category.show_add');
+        Route::post('/add', [CategoryController::class, 'addCategory'])->name('admin.category.add');
+        Route::post('/edit', [CategoryController::class, 'editCategory'])->name('admin.category.edit');
+        Route::get('/edit/{id}', [CategoryController::class, 'showEditCategory'])->name('admin.category.show_edit');
+        Route::get('/delete/{id}', [CategoryController::class, 'deleteCategory'])->name('admin.category.delete');
+    });
 });
